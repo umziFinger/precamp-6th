3d9f06e35bff88d94e5fc55bb77cc35c1a6834e5
KJ Mac <kjmac@DTui-MacBookPro.local>
2021-08-20 18:04:08 +0900

    첫번째 잔디

---
+++ class/03-javascript/연습-if.js
@@ -0,0 +1,32 @@
+f(1+1===2){
+    // console.log("정답입니다!!")
+}
+// VM256:2 정답입니다!!
+undefined
+if(1+1===2){
+    // console.log("정답입니다!!")
+} else {
+    // console.log("틀렸습니다!!")
+}
+// VM336:2 정답입니다!!
+undefined
+if(1+1===3){
+    // console.log("정답입니다!!")
+// } else {
+    // console.log("틀렸습니다!!")
+}
+VM350:4 틀렸습니다!!
+// undefined
+let age = 13
+undefined
+age
+13
+
+if(age >= 20) {
+    console.log("성인입니다.")
+} else if(age>=8) {
+    console.log("학생입니다.")
+} else {
+    console.log("어린이입니다.")}
+// VM953:4 학생입니다.
+// undefined/
---
+++ class/03-javascript/연습-phoneAuth.js
@@ -0,0 +1,24 @@
+Math.max(1,3,2)
+// 3
+Math.min(1,3,2)
+// 1
+Math.random()
+// 0.05580860776359775
+Math.floor(Math.random()*1000000)
+// 320956
+String(2356)
+// "2356"
+String(2356).padStart(6,"0")
+// "002356"
+Math.random()*1000000
+// 238070.76104762292
+Math.floor(Math.random()*1000000)
+// 44541
+String(Math.floor(Math.random()*1000000))
+// "513930"
+String(Math.floor(Math.random()*1000000)).padStart(6,0)
+// "098056"
+const token = String(Math.floor(Math.random()*1000000))
+// undefined
+token
+// "393322"
